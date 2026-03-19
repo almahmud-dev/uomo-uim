41cfce26e7341319f2c02be1b27f64ca0d218a1a
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-03-19 23:42:10 +0600

    CookieConsent some chnage

--- src/component/common/CookieConsent.jsx
+++ src/component/common/CookieConsent.jsx
@@ -7,7 +7,7 @@ const CookieConsent = () => {
   if (!visible) return null;
 
   return (
-    <div className="fixed bottom-4 right-3 z-50 w-[calc(100%-24px)] bg-head rounded p-4 shadow-[0_8px_32px_rgba(0,0,0,0.25)] sm:w-[300px] sm:bottom-6 sm:right-6 sm:p-6">
+    <div className="fixed bottom-4 right-3 z-50 w-[calc(100%-24px)] bg-head rounded p-4 shadow-[0_8px_32px_rgba(0,0,0,0.25)] sm:w-75 sm:bottom-6 sm:right-6 sm:p-6">
       {/* Text */}
       <p className="texts_14_regular text-white/90 mb-4 sm:mb-5">
         In order to provide you a personalized shopping experience, our site
@@ -23,7 +23,7 @@ const CookieConsent = () => {
       {/* Accept Button */}
       <button
         onClick={() => setVisible(false)}
-        className="w-full bg-second/30 hover:bg-second/50 transition-colors duration-200 rounded py-3 sm:py-[14px] texts_13_regular text-white font-semibold tracking-[2px] uppercase cursor-pointer"
+        className="w-full bg-second/30 hover:bg-second/50 transition-colors duration-200 rounded py-3 sm:py-3.5 texts_13_regular text-white font-semibold tracking-[2px] uppercase cursor-pointer"
       >
         Accept
       </button>
