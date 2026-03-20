795ffe144aa1bcf3ac90e9a2aa979fa0e636386a
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-03-20 17:56:06 +0600

    Firbase file edit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,17 @@
-import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyASOREp-nWaLziuC0y1iH3UpLqs8bSsfmQ",
+  authDomain: "uomo-uim-891e4.firebaseapp.com",
+  projectId: "uomo-uim-891e4",
+  storageBucket: "uomo-uim-891e4.firebasestorage.app",
+  messagingSenderId: "699609380272",
+  appId: "1:699609380272:web:c16a60eb66fe9623d01275"
 };
 
-const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-export const auth = getAuth(app);
-export default app;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
