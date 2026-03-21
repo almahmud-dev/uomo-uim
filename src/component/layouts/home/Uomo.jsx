2d90d61d2bb01c625530d011402c4c9804728129
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-03-21 10:40:31 +0600

    Uomo

--- src/component/layouts/home/Uomo.jsx
+++ src/component/layouts/home/Uomo.jsx
@@ -7,11 +7,11 @@ const image = "/assets/images/uomoImg.png";
 const Uomo = () => {
   return (
     <>
-      <section className="mt-[50px] lg:mt-[81px]">
+      <section className="mt-12.5 lg:mt-20.25">
         <Container>
           <div className="">
             <h2 className="head_35_regular text-bold text-center">@UOMO</h2>
-            <div className="mt-8.5 grid grid-cols-3 lg:grid-cols-6 gap-[15px]  lg:gap-[6px]">
+            <div className="mt-8.5 grid grid-cols-3 lg:grid-cols-6 gap-3.75 lg:gap-1.5">
               {[...new Array(12)].map((_, index) => {
                 return (
                   <div key={index}>
