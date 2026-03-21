c2e7bdf0bdce3f5c3743029f52d67907fb0a4bd8
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-03-21 10:38:20 +0600

    NotFound

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -13,13 +13,13 @@ const NotFound = () => {
     >
       <h1 className="font-bold text-[65px] lg:text-[100px] text-head">OOPS!</h1>
       <p className="pb-3 head_26_regular text-head">Page not found.</p>
-      <p className="pb-3 head_14_regular text-head max-w-[476px] text-center pb-6">
+      <p className="head_14_regular text-head max-w-119 text-center pb-6">
         Sorry, we couldn't find the page you where looking for. We suggest that
         you return to home page.
       </p>
       <Button
         btnText={"GO BACK"}
-        className={"bg-head  text-white py-5 px-[70px]  rounded lg:rounded-[0] lg:px-[140px] "}
+        className={"bg-head  text-white py-5 px-17.5  rounded lg:rounded-none lg:px-35"}
       />
     </div>
   );
