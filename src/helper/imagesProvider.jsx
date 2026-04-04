a000b56bfcaffb22de97bedb3a930df4aacd96dd
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-04-04 22:47:07 +0600

    ImagePorvider er file er image remove

--- src/helper/imagesProvider.jsx
+++ src/helper/imagesProvider.jsx
@@ -5,7 +5,6 @@ const allImages = {
   notFoundPage: "/assets/images/not-fund-img.jpg",
   addtocart: "/assets/images/ssd.png",
   navtabImg: "/assets/images/nav-tabs2.avif",
-  footerPaymenetImg: "/assets/images/footer-payment.png",
   shopimg: "/assets/images/insideBlog.png",
 };
 
