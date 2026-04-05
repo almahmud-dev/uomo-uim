0b835e77b6225bd1aad6241895b63905adfad998
alma-mahmud <alma.mahmud.dev@gmail.com>
2026-04-05 22:54:18 +0600

    next.config.mjs image optimised

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,8 @@ const nextConfig = {
 
   images: {
     formats: ["image/avif", "image/webp"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
+    imageSizes: [16, 32, 64, 128, 256],
     remotePatterns: [
       { protocol: "https", hostname: "dummyjson.com" },
       { protocol: "https", hostname: "cdn.dummyjson.com" },
